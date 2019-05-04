1164216f6e79d7c04d32549aadc2431e8ed650f9
AiaRup <aiale88@gmail.com>
2019-05-04 21:37:42 +0200

    lady bird ready, add mixins

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,10 +1,9 @@
 'use strict';
 
-const kissBtn = document.querySelector('.kiss__btn');
+const kissBtn = document.querySelector('.love-btn');
 const page = document.querySelector('.page');
 
 let heartArr = [];
-let isRaining = false;
 let screenWidth = document.documentElement.clientWidth;
 let counter = 0;
 let temp;
@@ -13,13 +12,13 @@ const createHeart = () => {
   const heart = document.createElement('div');
   heart.classList.add('heart');
   const speed = Math.floor(Math.random() * 16) + 5;
-  const position = Math.floor(Math.random() * (screenWidth - 50)) + 1;
+  const position = Math.floor(Math.random() * screenWidth) + 1;
   const anim = Math.floor(Math.random() * 2) + 1;
-  heart.style.animation = `falling${anim} ${speed}s 10`;
+  heart.style.animation = `falling${anim} ${speed}s ease-out infinite`;
   heart.style.left = `${position}px`;
   page.appendChild(heart);
   counter++;
-  if (counter === 30) {
+  if (counter === 20) {
     clearInterval(temp);
   }
 };
@@ -29,7 +28,6 @@ const setup = () => {
 };
 
 const kissMe = () => {
-  isRaining = true;
   setup();
 };
 
